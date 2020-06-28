76d3995c86a299246b1ffa0a3b44d9c0b93061d2
彭亮 <517306304@qq.com>
2020-06-28 13:45:58 +0800

    Update BitSetUtil.java

--- src/main/java/com/dragon/core/lang/bit/BitSetUtil.java
+++ src/main/java/com/dragon/core/lang/bit/BitSetUtil.java
@@ -10,18 +10,22 @@ package com.dragon.core.lang.bit;
 public final class BitSetUtil {
 
     public static int set(int option, int n) {
+        Assert.isTrue(n < Integer.SIZE, "the n must less than Integer.size");
         return (option | (1 << n));
     }
 
     public static long set(long option, int n) {
+        Assert.isTrue(n < Long.SIZE, "the n must less than Long.size");
         return (option | (1L << n));
     }
 
     public static int unset(int option, int n) {
+        Assert.isTrue(n < Integer.SIZE, "the n must less than Integer.size");
         return (option & ~(1 << n));
     }
 
     public static long unset(long option, int n) {
+        Assert.isTrue(n < Long.SIZE, "the n must less than Long.size");
         return (option & ~(1L << n));
     }
 
